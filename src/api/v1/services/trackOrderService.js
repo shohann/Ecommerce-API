37cfef0459f96d6c7ff10b8c8b8009df71f28a8b
shohann <shohanurr490@gmail.com>
2023-02-27 16:38:23 +0600

    track controller added

--- src/api/v1/services/trackOrderService.js
+++ src/api/v1/services/trackOrderService.js
@@ -1,6 +1,5 @@
 const { TrackOrder } = require('../models/DBInit');
 
-// get track order
 module.exports.fetchTrackByOrderId = async (orderId) => {
     return await TrackOrder.findUnique({
         where: {
